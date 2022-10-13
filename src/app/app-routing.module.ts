0a73ab0a0cc9fd90d394d7ae2e50668523bf08aa
Panut <63021710@up.ac.th>
2022-10-13 13:44:57 +0700

    Create component and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentComponent } from './component/component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app-component',component: ComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
