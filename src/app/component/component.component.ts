77dfa482111da5c7b1d2178042a30fdaf3f79e68
Panut <63021710@up.ac.th>
2022-10-15 20:12:51 +0700

    Get data base Loop data and Log data

--- src/app/component/component.component.ts
+++ src/app/component/component.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, Firestore, DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-component',
   templateUrl: './component.component.html',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const app = initializeApp(environment.firebaseConfig);
+    const db = getFirestore(app);
+
+    // async = รอข้อมูลมาครบก่อน ถึงจะเอามาใช้ได้
+    async function getMySelf(db: Firestore) {
+      const myself_col = collection(db, 'myself')
+      const myself_docs = await getDocs(myself_col)// await = ดึง document ครบ ถึงจะเอามาใช้ได้
+      return myself_docs
+    }
+    function showData(myself_data: QueryDocumentSnapshot<DocumentData>) {
+      console.log(myself_data.data());
+      // console.log(myself_data.data()['first_name']);
+    }
+
+    const data = await getMySelf(db)
+    data.forEach(myself_data => {
+      showData(myself_data)
+    })
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from "firebase/firestore";
 
 export const environment = {
   production: false,
@@ -16,9 +14,6 @@ export const environment = {
     measurementId: "G-T98FBZ42L9"
   }
 };
-
-const app = initializeApp(environment.firebaseConfig);
-const db = getFirestore(app);
 
 /*
  * For easier debugging in development mode, you can import the following file
